8a67a3fb7abd0e45c94af66c6c0040da8f460467
William McLean <w.mclean@unsw.edu.au>
2024-08-30 11:43:39 +1000

    plot of expected value

---
+++ figures/fig2.jl
@@ -0,0 +1,22 @@
+using PyPlot, JLD2, SubdiffusionQMC
+
+include(joinpath("..", "tables", "input_data.jl"))
+N = Nvals[6]
+soln_file = joinpath("..", "tables", "soln_$N.jld2")
+if isfile(soln_file)
+    println("Loading solution from $soln_file.\n")
+    EL = load(soln_file, "EL")
+    VL = load(soln_file, "VL")
+else
+    error("File %s not found.")
+end
+
+σ = sqrt.(VL)
+figure(2)
+plot(t, EL, t, EL+3σ, ":", t, EL-3σ, ":")
+grid(true)
+xlabel(L"$t$")
+#title(L"The expected value of $\mathcal{L}(u(t))$")
+legend((L"$E(t)$", L"$E(t)+3\sigma(t)$", L"$E(t)-3\sigma(t)$"))
+axis([-0.1, t[Nₜ]+0.1, 0, 2])
+savefig("fig2.pdf")
